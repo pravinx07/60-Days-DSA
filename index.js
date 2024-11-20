7c40732c94bbe1bf07bb83ef154d7439260533e6
Praving <shegamwarpravin@gmail.com>
2024-11-20 16:35:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1. Write a program to add an element to the end of an array.
+/*
+let students = ['pravin','Ajay','vijay']
+students.push("Aj")
+console.log(students);*/
+
+// 2. Write a program to add an element to the beginning of an array.
+
+/*let students = ['pravin','Ajay','vijay']
+students.unshift("vjj")
+console.log(students);*/
+
+// 3. Write a program to remove an element from the end of an array.
+
+let students = ["pravin", "Ajay", "vijay"];
+/*students.pop()
+console.log(students);*/
+
+// 4. Write a program to remove an element from the beginning of an array.
+
+/*students.shift()
+console.log(students);*/
+
+// 5. Write a program to find the length of an array.
+// console.log(students.length);   // property that doesnt required () => length
+
+// 6. Write a program to loop through all the elements of an array using a for loop.
+
+/*for(let i = 0; i < students.length; i++){
+    console.log(i,students[i]);
+    
+}
+*/
