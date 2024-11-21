b5460088e1f709770f6cbb52dfaf8415721dfc2c
Praving <shegamwarpravin@gmail.com>
2024-11-21 15:35:24 +0530

    Day-2

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let students = ['pravin','Ajay','vijay']
 students.push("Aj")
 console.log(students);*/
 
+const e = require("express");
+
 // 2. Write a program to add an element to the beginning of an array.
 
 /*let students = ['pravin','Ajay','vijay']
@@ -31,3 +33,48 @@ console.log(students);*/
     
 }
 */
+
+// 7. Write a program to loop through all the elements of an array using a for...of loop.
+
+for(let element of students){
+    console.log(element);
+    
+}
+
+// 8. Write a program to loop through all the elements of an array using the forEach method.
+students.forEach(element => {    //use callback function
+    console.log(element);
+    
+});
+
+// 9. Write a program to modify an element in the array by its index.
+
+students[0] = 'Pravin07'
+console.log(students);
+
+
+// 10. Write a program to check if an element exists in an array.
+console.log(students.includes('pravin'));
+
+// 11. Write a program to find the index of an element in an array
+console.log(students.indexOf("vijay"));
+
+// 12. Write a program to create a new array using the map() method, where each element is doubled.
+
+let num = [1,2,3,4,5]
+let double = num.map((item)=>item*2)   //map required callback 
+    console.log(double);
+
+    // var names = ['ajay','pravin','vijay']
+
+     var nameCap = names.map((name)=>name.toUpperCase())
+    console.log(nameCap);
+    
+    
+    // 13. Write a program to create a new array using the filter() method, containing only even numbers from a given array.
+
+    var nums = [1,2,4,5,6,7,8]
+    let even = nums.filter((num)=>num % 2 == 0)
+    console.log(even);
+    
+    
