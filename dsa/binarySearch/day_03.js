e362a2bcb41ff77f27826d86f1058cba869c5bee
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-02-06 20:44:23 +0530

    first ocuurance and last occurance

---
+++ dsa/binarySearch/day_03.js
@@ -0,0 +1,89 @@
+/*Given an array of integers nums sorted 
+in non-decreasing order,
+ find the starting and 
+ ending position of a given target value.
+
+If target is not found in the array,
+ return [-1, -1].
+
+ Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+ */
+
+// function searchRange(arr,target){
+//     let n = arr.length;
+//     let first = -1;
+//     let last = -1;
+//     for(let i = 0; i < n; i++){
+//         if(arr[i] === target){
+//             if(first === -1) first = i
+//             last = i
+//         }
+//     }
+//     return new Array(first , last)
+// }
+// console.log(searchRange([5,7,7,8,8,10],8));
+// console.log(searchRange([5,7,7,8,8,10],6));
+
+// using binary search
+function firstOccurence(arr, target) {
+  let n = arr.length;
+  let low = 0;
+  let high = n - 1;
+  let first = -1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) {
+      first = mid;
+      high = mid - 1;
+    } else if (arr[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return first;
+}
+
+// console.log(firstOccurence([5, 7, 7, 8, 8, 10], 8));
+
+function lastOccurence(arr, target) {
+  let n = arr.length;
+  let low = 0;
+  let high = n - 1;
+  let last = -1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) {
+      last = mid;
+      low = mid + 1;
+    } else if (arr[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return last;
+}
+
+// console.log(lastOccurence([5, 7, 7, 8, 8, 10], 8));
+function findFirstAndLast(arr,target){
+    
+let firstOccu = firstOccurence(arr,target)
+if(firstOccu === -1) return [-1,-1];
+
+let lastOccu = lastOccurence(arr,target)
+return [firstOccu,lastOccu]
+
+}
+
+console.log(findFirstAndLast([5, 7, 7, 8, 8, 10], 6));
--- dsa/binarySearch/index_2.js
+++ dsa/binarySearch/index_2.js
@@ -4,24 +4,30 @@
 smallest index such that arr[ind] > k
  */
 
-function upperBound(arr,k){
-    let n = arr.length;
-    let low = 0;
-    let high = n - 1;
-    ans = n;
+// function upperBound(arr,k){
+//     let n = arr.length;
+//     let low = 0;
+//     let high = n - 1;
+//     ans = n;
 
-    while(low <= high){
-        let mid = Math.floor((low + high) / 2)
+//     while(low <= high){
+//         let mid = Math.floor((low + high) / 2)
 
-        if(arr[mid] > k) {
+//         if(arr[mid] > k) {
 
-            ans = mid
-            high = mid - 1
-        }else{
-            low = mid + 1
-        }
-    }
-    return ans;
-}
+//             ans = mid
+//             high = mid - 1
+//         }else{
+//             low = mid + 1
+//         }
+//     }
+//     return ans;
+// }
 
-console.log(upperBound([1,2,3,4,5,6,7,8],3));
+// console.log(upperBound([1,2,3,4,5,6,7,8],3));
+
+
+/**
+ ceil and floor
+ 
+ */
