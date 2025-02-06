d3c432581b1e4a8f7dbe9193169329a7ab1f572c
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-02-06 23:30:25 +0530

    search in rotated array

--- dsa/binarySearch/day_03.js
+++ dsa/binarySearch/day_03.js
@@ -145,3 +145,52 @@ function firstOccurence_2(arr, target) {
   
   console.log(findFirstAndLast_2([5, 7, 7, 8, 8, 10], 8));
   
+
+/*
+search in rotated sorted array
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+using linear search
+ */
+
+function searchNum(arr,target){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === target) return i
+    }
+    return -1;
+}
+
+console.log(searchNum([4,5,6,7,0,1,2],0));
+
+// binary search 
+
+var search = function(nums, target) {
+    let n = nums.length;
+   let low = 0;
+   let high = n - 1;
+
+   while(low <= high){
+    let mid = Math.floor((low + high) / 2)
+
+    if(nums[mid] === target) return mid;
+
+    // left sorted
+
+    if(nums[low] <= nums[mid]){
+        if(nums[low] <= target && target <= nums[mid]){
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }else{
+        if(nums[mid] <= target && target <= nums[high]){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+   }
+   return -1;
+
+};
