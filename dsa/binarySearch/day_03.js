0732ec90029260f484a952df8fec2a91c84272f3
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-02-06 21:09:54 +0530

    number of c=occurance

--- dsa/binarySearch/day_03.js
+++ dsa/binarySearch/day_03.js
@@ -87,3 +87,61 @@ return [firstOccu,lastOccu]
 }
 
 console.log(findFirstAndLast([5, 7, 7, 8, 8, 10], 6));
+
+
+// number of accurance
+function firstOccurence_2(arr, target) {
+    let n = arr.length;
+    let low = 0;
+    let high = n - 1;
+    let first = -1;
+  
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
+      if (arr[mid] === target) {
+        first = mid;
+        high = mid - 1;
+      } else if (arr[mid] < target) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return first;
+  }
+  
+  // console.log(firstOccurence([5, 7, 7, 8, 8, 10], 8));
+  
+  function lastOccurence_2(arr, target) {
+    let n = arr.length;
+    let low = 0;
+    let high = n - 1;
+    let last = -1;
+  
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
+      if (arr[mid] === target) {
+        last = mid;
+        low = mid + 1;
+      } else if (arr[mid] < target) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return last;
+  }
+  
+  // console.log(lastOccurence([5, 7, 7, 8, 8, 10], 8));
+  function findFirstAndLast_2(arr,target){
+      
+  let firstOccu = firstOccurence(arr,target)
+  if(firstOccu === -1) return 0;
+  
+  let lastOccu = lastOccurence(arr,target)
+  return (lastOccu - firstOccu) + 1
+  
+  }
+  
+  console.log(findFirstAndLast_2([5, 7, 7, 8, 8, 10], 8));
+  
