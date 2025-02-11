9918a2a97691c3beefbe0b77699d36fda5585063
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-02-11 20:25:09 +0530

    binary to decimal

--- dsa/binarySearch/BitManipulation/day_06.js
+++ dsa/binarySearch/BitManipulation/day_06.js
@@ -1,5 +1,6 @@
 // converting decimal to binary
 
+/*
 function decimalToBinary(n){
     res = ""
     while(n > 0){
@@ -15,3 +16,22 @@ function decimalToBinary(n){
 
 console.log(decimalToBinary(10));
 console.log(decimalToBinary(11));
+*/
+
+// binary to decimal
+
+function binaryToDecimal(num){
+  let len = num.length;
+  let decimalValue  = 0
+  let pow = 1
+    for(i = len - 1; i >= 0; i--){
+        if(num[i] === '1'){
+         decimalValue += pow
+        }
+        pow *= 2
+    }
+    return  decimalValue
+}
+
+console.log(binaryToDecimal('1010'));
+console.log(binaryToDecimal('1110'));
