c4bafad990407f2b57ff24b0a855bec13af1dadc
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-02-27 08:32:28 +0530

    find xor l to r optimal solution

--- dsa/binarySearch/BitManipulation/day_18.js
+++ dsa/binarySearch/BitManipulation/day_18.js
@@ -6,13 +6,22 @@ Output:
 Explanation:
 4 ^ 5 ^ 6 ^ 7 ^ 8 = 8*/
 
-function xorOp(l,r){
-    let ans = 0
-    for(let i = l; i <= r; i++){
-        
-        
-        ans = ans ^ i
-    }
-    return ans
+function oper1(n){
+   if(n % 4 === 1) return 1;
+   else if(n % 4 === 2) return n + 1;
+   else if (n % 4 === 3) return 0
+   else return n
 }
-console.log(xorOp(4,8));
+// console.log(oper1(10));
+
+function xorOp(l,r){
+    // let ans = 0
+    // for(let i = l; i <= r; i++){
+        
+        
+    //     ans = ans ^ i
+    // }
+    // return ans
+   return oper1(l - 1) ^ oper1(r)
+}
+console.log(xorOp(4,9));
