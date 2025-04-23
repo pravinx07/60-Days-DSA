2232848111830ad5e96b45afadc7efe77c55b6fc
MrPravinS <shegamwarpravin07@gmail.com>
2025-04-23 12:27:15 +0530

    starting sorting

---
+++ sorting/bubble.js
@@ -0,0 +1,14 @@
+let arr = [10, 5, 1, 12, 3];
+let n = arr.length;
+
+for (i = 0; i < n - 1; i++) {
+  for (let j = 0; j < n - 1 - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}
+
+console.log(arr);
