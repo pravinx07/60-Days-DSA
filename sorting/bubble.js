b996603dc54fb335b0416a842abaec983a96c902
MrPravinS <shegamwarpravin07@gmail.com>
2025-04-26 16:26:32 +0530

    sorting algorithm

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,5 +1,5 @@
 // bubble sort
-
+/*
 let arr = [10,5,12,1,3]
 let n = arr.length;
 
@@ -14,3 +14,33 @@ for(let i = 0; i< n - 1; i++){
 }
 
 console.log(arr);
+*/
+
+function bubbleSort(arr){
+    let n = arr.length
+    for(let i = n - 1; i >= 1; i--){
+        let didSwap = 0
+        for(let j = 0; j <= i; j++)
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                didSwap = 1
+            }
+            
+            if(didSwap === 0){
+                break
+            }
+            
+            console.log("Loop Runs");
+            
+        }
+    }
+
+const arr = [12,24,46,20,9,52]
+const arr2 = [1,2,4,5,6]
+bubbleSort(arr2)
+console.log("After Sorting: "+ arr2);
+bubbleSort(arr)
+console.log("After Sorting: "+ arr);
+
--- sorting/insertion.js
+++ sorting/insertion.js
@@ -1,5 +1,5 @@
 // insertion sort
-
+/*
 let arr = [10,5,12,1,3]
 let n = arr.length;
 
@@ -13,3 +13,22 @@ for(let i = 1; i < n; i++){
     arr[j+1] = key
 }
 console.log(arr);
+*/
+
+function insertionSort(arr){
+    let n = arr.length
+    for(i = 0; i <= n - 1; i++){
+        j = i
+        while(j > 0 && arr[j-1]>arr[j]){
+            let temp = arr[j]
+            arr[j] = arr[j-1]
+            arr[j-1] = temp
+            j--
+        }
+    }
+}
+
+let arr = [3,2,0,5,6]
+insertionSort(arr)
+console.log("After Sorting: "+ arr);
+
