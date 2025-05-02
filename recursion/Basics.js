8c045afd73acb0c7566572cd94a2df5c747bf700
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-02 19:08:56 +0530

    learn recursion

---
+++ recursion/Basics.js
@@ -0,0 +1,13 @@
+/* What is recursion
+   A function calling itself  until a specific condition is met 
+   base condition => Condition that terminate recursion
+   stackOverflow => stack is out of range means program is waiting in the stack for execution
+   recursion tree => f() => <=f() =><= f() =><= f() base condition stop and went back
+ */
+
+   function nTimes(n){
+    console.log("Printed",n);
+    setTimeout(()=>nTimes(2),1000)
+   }
+
+   nTimes()
