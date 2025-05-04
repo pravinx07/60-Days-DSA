df32be8db700438ab8b3592e96affbf0d417009f
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-04 13:03:55 +0530

    left rotated array by d place optimal

--- array/rotate2.js
+++ array/rotate2.js
@@ -2,7 +2,7 @@
 // arr = [1,2,3,4,5,6,7] d = 3
 // arr = [4,5,6,7,1,2,3]
 
-
+/*
 function rotateArr(arr,d){
   let n = arr.length
   d = d % n
@@ -25,4 +25,30 @@ let arr = [1,2,3,4,5,6,7]
 let d = 8
 console.log(rotateArr(arr,d));
 
+*/
 
+// using reversed arr
+
+function reverseArr(arr,start,end){
+    while(start <= end){
+        let temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start++
+        end--
+     }
+}
+
+
+function main(arr,d){
+    let n = arr.length;
+    reverseArr(arr,0,d-1)
+    reverseArr(arr,d,n-1)
+    reverseArr(arr,0,n-1)
+    return arr
+}
+
+let arr = [1,2,3,4,5,6,7]
+let d = 4
+main(arr,d)
+console.log("Array Rotated: ",arr);
