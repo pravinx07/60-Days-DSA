ae3410f3e6cf5d5dddfa01284236ab3a1b0d29f8
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-04 13:15:49 +0530

    right rotated array

--- array/rotate2.js
+++ array/rotate2.js
@@ -29,6 +29,7 @@ console.log(rotateArr(arr,d));
 
 // using reversed arr
 
+/*
 function reverseArr(arr,start,end){
     while(start <= end){
         let temp = arr[start]
@@ -52,3 +53,44 @@ let arr = [1,2,3,4,5,6,7]
 let d = 4
 main(arr,d)
 console.log("Array Rotated: ",arr);
+*/
+
+// rotate right by d place
+// arr = [1,2,3,4,5,6,7] => d = 1
+//    arr = [7,1,2,3,4,5,6], d = 2
+    //  arr = [6,7,1,2,3,4,5], d = 3
+        // arr = [5,6,7,1,2,3,4]
+
+function reverse(arr,start,end){
+    while(start < end){
+        let temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start++
+        end--
+        
+        
+    }
+}
+
+function rotatedRight(arr,d){
+    let n = arr.length;
+    d = d % n
+    reverse(arr,0,n-1)
+    console.log("reverse", arr);
+    
+    reverse(arr,0,d-1)
+    console.log("reverse", arr);
+
+    reverse(arr,d,n-1)
+    console.log("reverse", arr);
+
+
+    return arr
+}
+
+
+let arr = [1,2,3,4,5,6,7]
+let d = 3
+rotatedRight(arr,d)
+console.log(arr);
