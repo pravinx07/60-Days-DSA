e06a6fd58eb9c54f4230acd6c49b498807a97e06
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-06 13:05:54 +0530

    sort array using two pointer

--- array/union.js
+++ array/union.js
@@ -4,6 +4,7 @@ arr2 = [1,3,2,4,5]
 union = [1,2,3,4,5]
 
 */
+/*
 function unionOfArr(arr1,arr2){
     let n = arr1.length
     let m = arr2.length
@@ -28,3 +29,51 @@ let arr1 = [1,2,2,1,3,4]
 let arr2 = [1,3,2,4,5]
 console.log("Union Of Two Arrays:" , unionOfArr(arr1,arr2))
 
+*/
+
+//  two pointer approch optimal
+
+function unionOfTwoArr(arr1,arr2){
+    // arr1.sort()
+    // arr2.sort()
+    let n = arr1.length
+    let m = arr2.length
+    let i = 0
+    let j = 0
+    let union = []
+
+    while(i < n && j < m){
+        if(arr1[i] <= arr2[j]){ 
+
+            if(union.length === 0 || union[union.length - 1] !== arr1[i]){
+                union.push(arr1[i])
+            }
+            i++
+        }else{
+            if(union.length === 0 || union[union.length - 1] !== arr2[j]){
+                union.push(arr2[j])
+            }
+            j++
+        }
+    }
+
+    while(j < m){
+        if(union.length === 0 || union[union.length - 1] !== arr2[j]){
+            union.push(arr2[j])
+        }
+        j++
+    }
+
+    while(i < n){
+        if(union.length === 0 || union[union.length - 1] !== arr1[i]){
+            union.push(arr1[i])
+        }
+        i++
+    }
+
+    return union
+}
+
+let arr1 = [1,2,2,3,3,4]
+let arr2 = [1,3,3,4,5]
+console.log("Union Of Two Arrays:" , unionOfTwoArr(arr1,arr2))
