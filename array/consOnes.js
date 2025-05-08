52c7f24d3527d63b49991c67fd27e0f869842510
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-08 10:45:45 +0530

    maximum consecutive ones

---
+++ array/consOnes.js
@@ -0,0 +1,24 @@
+/**
+ maximum consecutive ones 
+  arr = [1,1,0,1,1,1,0,1,1]
+  ans = 3 => bcos consecutives ones are 3
+ */
+
+  function consecutiveOnes(arr){
+    let cnt = 0
+    let maxi = 0
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 1){
+            cnt++
+            maxi = Math.max(maxi,cnt)
+        }else{
+            cnt = 0
+        }
+    }
+    return maxi
+  }
+
+  let arr = [1,1,0,1,1,1,0,1,1]
+
+  console.log(consecutiveOnes(arr))
+  
