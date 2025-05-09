336cde1a2d69f4354e529a8f427879271252d83c
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-09 19:44:28 +0530

    longest sub array with given sum k brute

---
+++ array/sumK.js
@@ -0,0 +1,26 @@
+/*
+write a program that give longest subarray of given sum k positive
+arr = [1,2,4,1,1,1,4,2] k = 3
+ans = 1,1,1 => 3
+*/
+
+
+function subArrSum(arr,num){
+    let len = 0
+    let n = arr.length
+    for(let i = 0; i < n; i++){
+       for(let j = i; j < n; j++){
+        sum = 0
+        for(k = i; k < j; k++){
+            sum += arr[k]
+            if(sum === num) return Math.max(len,j-i+1)
+        }
+       }
+    }
+    console.log(len);
+    
+}
+
+let arr = [1,2,3,1,1,4,5]
+let num = 2
+console.log(subArrSum(arr,num));
