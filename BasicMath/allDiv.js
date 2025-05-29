a707b15291499961f7145df048df2ff362351d3d
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-29 09:22:36 +0530

    print all divisors using math

--- BasicMath/allDiv.js
+++ BasicMath/allDiv.js
@@ -2,6 +2,7 @@
 ex => 36 => [1,2,3,4,6,9,12,18,36]
  */
 
+/*
 function allDivisors(n){
     let arr = []
     for(let i = 1; i <= n; i++){
@@ -17,3 +18,20 @@ function allDivisors(n){
 
 allDivisors(36)
 allDivisors(35)
+*/
+
+function printAllDiv(n){
+    let arr = []
+
+    for(let i = 1; i <= Math.sqrt(n); i++){
+        if(n % i === 0){
+            arr.push(i)
+            if(Math.floor(n / i) !== i){
+                arr.push(n/i)
+            }
+        }
+    }
+    return arr.sort((a,b)=>a-b)
+}
+
+console.log(printAllDiv(36))
