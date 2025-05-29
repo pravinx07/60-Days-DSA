51f8e268e47ee7d114b05fd5e719b60b7565c000
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-29 09:38:38 +0530

    prime number optimal

--- BasicMath/primeNo.js
+++ BasicMath/primeNo.js
@@ -4,14 +4,17 @@
 
  function isPrim(n){
     let count = 0;
-    for(let i = 1; i <= n; i++){
+    for(let i = 1; i*i <= n; i++){
         if(n % i === 0){
             count++
+            if(Math.floor(n / i) !== i){
+                count++
+            }
         }
     }
     if(count === 2) return true
     return false
  }
 
- console.log(isPrim(6));
+ console.log(isPrim(24));
  
