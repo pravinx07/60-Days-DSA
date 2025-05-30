c62b5f1a960a843340783bef79d308f8fece09f0
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-30 09:44:21 +0530

    ginf GCD using equiladian triangle

--- BasicMath/gcd.js
+++ BasicMath/gcd.js
@@ -4,9 +4,10 @@
   ans = common and greatest is => 3
  */
 
-  function findGCD(n1,n2){
+  /*
+  function findGCD(n1,n2,n3){
     let gcd = 1
-    for(let i = 1; i <= Math.min(n1,n2); i++){
+    for(let i = 1; i <= Math.min(n1,n2,n3); i++){
         if(n1 % i === 0 && n2 % i === 0){
             gcd = i
         }
@@ -14,8 +15,25 @@
     return gcd
   }
 
-  console.log(findGCD(12,1));
-  console.log(findGCD(9,12));
-  console.log(findGCD(11,13));
-  console.log(findGCD(20,40));
+  console.log(findGCD(12,1,2));
+  console.log(findGCD(9,12,24));
+  console.log(findGCD(11,13,17));
+  console.log(findGCD(20,40,60));
+  */
   
+
+  function findGCD(n1,n2){
+    while(n1 > 0 && n2 > 0){
+        if(n1 > n2) n1 = n1 % n2
+        else n2 = n2 % n1
+    }
+    if(n1 === 0) console.log(n2);
+    else console.log(n1);
+    
+    
+  }
+
+
+  findGCD(9,12)
+  findGCD(11,13)
+  findGCD(20,40)
