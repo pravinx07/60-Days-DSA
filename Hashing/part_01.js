254523e29af2dba65f2be30df0b532cff2d75374
MrPravinS <shegamwarpravin07@gmail.com>
2025-06-02 09:36:10 +0530

    map methods get, set, has, delete etc

--- Hashing/part_01.js
+++ Hashing/part_01.js
@@ -1,14 +1,40 @@
-function map(arr){
-    let mp = new Map()
+// function map(arr){
+//     let mp = new Map()
     
-    for(let num of arr){
-        mp.set(num,(mp.get(num) || 0) + 1)
-    }
-    return mp
+//     for(let num of arr){
+//         mp.set(num,(mp.get(num) || 0) + 1)
+//     }
+//     return mp
+// }
+
+// let arr = [1,2,3,4,2,1,3]
+// console.log(map(arr));
+
+
+
+let map = new Map()
+map.set("Name","Pravin")
+map.set("age",23)
+map.set("Job","Hr")
+
+console.log(map);
+
+console.log(map.get("Name"),map.size)
+console.log(map.has("Name"))
+map.delete("Job")
+
+map.forEach((key,val)=>{
+    console.log(key, " => ", val);
+    
+})
+
+for(let [key,valu] of map){
+    console.log(`${key} => ${valu}`);
+    
 }
 
-let arr = [1,2,3,4]
-console.log(map(arr));
-
+console.log(map);
+map.clear()
+console.log(map, map.size);
 
 
