71ecf8f6b93bf112b1142648c42c07517cbc7ccf
MrPravinS <shegamwarpravin07@gmail.com>
2025-06-06 09:34:19 +0530

    insert at specific index and linkedlist convert into array

--- LinkedList/basics.js
+++ LinkedList/basics.js
@@ -33,45 +33,87 @@ class LinkedList {
     this.length++;
   }
 
-  prepend(value){
-    const newNode = new Node(value)
-    newNode.next = this.head
-    this.head = newNode
-    this.length++
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
   }
 
-  find(value){
-    let current = this.head
-    while(current){
-        if(current.value === value){
-            return current
-        }
-        current = current.next
+  find(value) {
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        return 1;
+      }
+      current = current.next;
     }
-    return null
+    return -1;
   }
 
-  delete(value){
-    if(!this.head) return 
+  delete(value) {
+    if (!this.head) return;
 
-    if(this.head.value === value){
-        this.head = this.head.next;
-        this.length--;
-        return;
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      this.length--;
+      return;
     }
 
     let current = this.head;
-    while(current.next && current.next.value !== value){
-     current = current.next;
+    while (current.next && current.next.value !== value) {
+      current = current.next;
     }
 
-    if(current.next){
-        current.next = current.next.next;
-        this.length--;
+    if (current.next) {
+      current.next = current.next.next;
+      this.length--;
     }
-    
   }
 
+  insertAt(index, value) {
+    if(index < 0 || index > this.length) {
+      console.log("Invalid Index ");
+      return
+    }
+    let newNode = new Node(value)
+    let current = this.head
+    let previous = null
+    let count = 0;
+
+    if(index === 0){
+      newNode.next = this.head
+      this.head = newNode
+    }else{
+      while(count < index){
+        previous = current
+        current = current.next
+        count++
+      }
+      newNode.next = current
+      previous.next = newNode
+    }
+
+    this.length++
+
+  }
+
+  toArray() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+      // console.log(arr);
+    }
+    // console.log(arr);
+    return arr;
+  }
+
+  isAEmpty() {
+    if (!this.length) return true;
+    return false;
+  }
   print() {
     let current = this.head;
     let result = "";
@@ -81,18 +123,21 @@ class LinkedList {
     }
 
     console.log(result + "null");
-    console.log("Length of linkedlist is ", this.length);
+    // console.log("Length of linkedlist is ", this.length);
   }
 }
 
 const list = new LinkedList();
-list.prepend(10)
-list.prepend(6)
+list.prepend(10);
+list.prepend(6);
 list.append(32);
 list.append(2);
 
-
-list.delete(21)
-// list.find(3)
+// list.delete(21)
 // console.log(list.find(21))
+list.insertAt(3,9);
 list.print();
+// console.log(list.find(6))
+// console.log(list.toArray())
+// console.log("is Empty", list.isAEmpty());
+
