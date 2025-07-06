f1a6d02ba5a44ca8f58a588a58c23d0d1456ea3d
MrPravinS <shegamwarpravin07@gmail.com>
2025-07-06 23:09:10 +0530

    learn binary search optimal aproch

--- array/bSearch.js
+++ array/bSearch.js
@@ -2,14 +2,28 @@
 // arr = [1,2,3,4,5,6] target = 5 
 // return index of target arr else -1 
 
-function binarySearch(arr, target){
-    for(let i = 0; i < arr.length; i++)
-    {
-        if(target === arr[i]) return i
+// function binarySearch(arr, target){
+//     for(let i = 0; i < arr.length; i++)
+//     {
+//         if(target === arr[i]) return i
 
+//     }
+//     return -1
+// }
+
+function binarySearch(arr, target){
+    let low = 0
+    let high = arr.length - 1;
+
+    while(low <= high){
+        let mid = Math.floor((low + high) / 2)
+
+        if(arr[mid] === target) return mid;
+        else if(arr[mid] < target) low = mid + 1
+        else high = mid - 1
     }
     return -1
 }
 
 let arr = [1,2,3,4,5,6]
-console.log(binarySearch(arr, 8));
+console.log(binarySearch(arr, 2));
