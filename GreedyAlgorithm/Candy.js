acb3c5498e784942e2a7a06bb00eb4325ecdcad8
MrPravinS <shegamwarpravin07@gmail.com>
2025-10-17 08:13:46 +0530

    using 0ne pass

--- GreedyAlgorithm/Candy.js
+++ GreedyAlgorithm/Candy.js
@@ -15,30 +15,62 @@ Explanation: You can allocate to the first, second and third child with 1, 2, 1 
 The third child gets 1 candy because it satisfies the above two conditions.
 
 */
-var candy = function(arr) {
-    let n = arr.length;
-    let ltr = Array(n).fill(1);
+// var candy = function(arr) {
+//     let n = arr.length;
+//     let ltr = Array(n).fill(1);
 
-    for(let i = 1; i < n; i++){
-        if(arr[i] > arr[i - 1]){
-            ltr[i] = ltr[i - 1] + 1;
-        }
+//     for(let i = 1; i < n; i++){
+//         if(arr[i] > arr[i - 1]){
+//             ltr[i] = ltr[i - 1] + 1;
+//         }
+//     }
+
+//     let rtl = Array(n).fill(1);
+//     for(let i = n - 2; i >= 0; i--){
+//         if(arr[i] > arr[i + 1]){
+//             rtl[i] = rtl[i + 1] + 1;
+//         }
+//     }
+
+//     let ans = 0
+//     for(let i = 0; i < n; i++){
+//         ans = ans + Math.max(ltr[i],rtl[i])
+//     }
+//     return ans;
+// };
+
+
+var candy = function(ratings) {
+    let n = ratings.length;
+    let ans =  n;
+    let i = 1;
+
+    while(i < n){
+    while(i < n && ratings[i] === ratings[i-1]){
+        i++;
+        continue;
+    }
+    let up = 0;
+    while(i < n && ratings[i] > ratings[i - 1]){
+        ++up;
+        ans = ans + up;
+        i++;
     }
 
-    let rtl = Array(n).fill(1);
-    for(let i = n - 2; i >= 0; i--){
-        if(arr[i] > arr[i + 1]){
-            rtl[i] = rtl[i + 1] + 1;
-        }
+    let down = 0;
+    while( i < n && ratings[i] < ratings[i - 1]){
+        ++down;
+        ans += down;
+        i++
     }
 
-    let ans = 0
-    for(let i = 0; i < n; i++){
-        ans = ans + Math.max(ltr[i],rtl[i])
+    ans = ans - Math.min(up,down)
+
     }
     return ans;
 };
-
-
 let ratings = [1,0,2]
 console.log(candy(ratings));
+
+
+
