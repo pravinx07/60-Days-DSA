905490c1cc9a298890d7754acf8d2e0dab890c90
MrPravinS <shegamwarpravin07@gmail.com>
2025-10-19 16:25:46 +0530

    Min cost climbing Stairs

---
+++ DP/minCostClimbingStairs.js
@@ -0,0 +1,13 @@
+var minCostClimbingStairs = function(cost) {
+    let n = cost.length;
+    let dp = [0,0];
+
+    for(let i = 2; i <= n; i++){
+        dp[i] = Math.min(dp[i - 1] + cost[i - 1] , dp[i - 2] + cost[i - 2])
+
+    }
+    return dp[n];
+};
+
+let cost = [10,15,20]
+console.log(minCostClimbingStairs(cost));
